2ec428b61a8f272472f073b419542a25562fe12b
Raj <rajdipshah01!gmail.com>
2021-08-23 10:39:12 +0545

    yup 23r august

--- JAVASCRIPT/home.js
+++ JAVASCRIPT/home.js
@@ -1,6 +1,22 @@
-console.log('hello');
-var f = "Nah" ;
-var n = 10 ;
-console.log(n);
-alert(f); 
-document.getElementById("GREETINGS").innerHTML = 'Waddup' ;   
+// console.log('hello');
+// var f = "Nah" ;
+// var n = 10 ;
+//console.log(n);
+// alert(f); 
+function greet (name =prompt("Your name ") ){
+console.log('Sup ' + name ) ;
+document.getElementById("GREETINGS").innerHTML = ('Waddup ' + name) ; 
+}
+// greet()
+ 
+// DATA TYPES 
+let age = 16 ; // number 
+let first_name = 'Rajdip' ;// string
+let drinking_age = false ; // Boolean 
+let countries = {Nepal : "Kathmandu", US : "Washington DC"} ; // Objects ~known as ictionary in python~
+let allies = ['US' ," UK ", "FRANCE", "CANADA"] ; // arrays ~known as lists in python
+let name ; // Undefined 
+let goals = null ;// value null
+console.log(first_name.length) ;
+console.log(first_name.slice(2, 5)) ;
+ 
